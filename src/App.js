a2547eaf4fba17e628f784f5772b17075ab3ae7e
jihyuunn <791004_@likelion.org>
2020-03-24 22:35:57 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'tui-image-editor/dist/tui-image-editor.css';
+import ImageEditor from '@toast-ui/react-image-editor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageEditor 
+        includeUI={{
+          loadImage: {
+            path:'public/ex.png',
+            name: 'sample'
+          },
+          menu: ['shape', 'filter'],
+          initMenu: 'filter',
+          uiSize: {
+            width: '1000px',
+            height: '700px'
+          },
+          menuBarPosition: 'bottom'
+        }}
+        cssMaxHeight={500}
+        cssMaxWidth={700}
+        selectionStyle={{
+          cornerSize: 20,
+          rotatingPointOffset: 70
+        }}
+        />
     </div>
   );
 }
